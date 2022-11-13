d18e098774980fa17540954bd6ff3912ea2e785f
Alcaen <alejocaicedoenriquez@gmail.com>
2022-11-13 11:44:19 -0500

    Note Status Handled With Text Overflow Fixed

--- pages/ideas/index.tsx
+++ pages/ideas/index.tsx
@@ -65,11 +65,45 @@ const AllIdeas: React.FC<Props> = () => {
             <title>All Ideas</title>
           </Head>
           <div className='mx-4 my-5'>
-            <h1 className='text-3xl font-semibold mx-3'>All Ideas</h1>
+            <div className='flex items-center break-keep space-x-4 '>
+              <h1 className='text-3xl font-semibold mx-3 inline'>All Ideas</h1>
+              <div>
+                <input
+                  className='appearance-none rounded-full h-4 w-4 border border-red-600 bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer'
+                  type='radio'
+                  value='todo'
+                  name='status'
+                  // checked={status == 'todo'}
+                  // onChange={(e) => setStatus(e.target.value)}
+                />
+                <label className='font-semibold whitespace-nowrap'>To Do</label>
+              </div>
+              <div>
+                <input
+                  className='appearance-none rounded-full h-4 w-4 border border-yellow-400 bg-white checked:bg-yellow-400 checked:border-yellow-400 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer'
+                  type='radio'
+                  value='inprogress'
+                  name='status'
+                  // onChange={(e) => setStatus(e.target.value)}
+                />
+                <label className='font-semibold'>In Progress</label>
+              </div>
+              <div>
+                <input
+                  className='appearance-none rounded-full h-4 w-4 border border-green-600 bg-white checked:bg-green-600 checked:border-green-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer'
+                  type='radio'
+                  value='done'
+                  name='status'
+                  // onChange={(e) => setStatus(e.target.value)}
+                />
+                <label className='font-semibold'>Done</label>
+              </div>
+            </div>
+
             <div className='flex justify-evenly flex-wrap w-full'>
               <Link
                 href={'/ideas/create'}
-                className='bg-green-600 w-[160px] h-[150px] flex-col my-8 p-4 rounded-3xl drop-shadow-lg text-white font-semibold hover:bg-green-500 hover:-translate-y-5 hover:-translate-x-5 transition  cursor-pointer'
+                className='bg-cyan-600 w-[160px] h-[150px] flex-col my-8 p-4 rounded-3xl drop-shadow-lg text-white font-semibold hover:bg-cyan-500 hover:-translate-y-5 hover:-translate-x-5 transition  cursor-pointer'
               >
                 <p className='text-lg block text-center'>Add New</p>
                 <p className='text-8xl -mt-3 text-center font-light'>{'+'}</p>
@@ -79,10 +113,19 @@ const AllIdeas: React.FC<Props> = () => {
                   <Link
                     href={`/ideas/${idea.title}`}
                     key={idea.title}
-                    className='bg-cyan-600 w-[160px] h-[150px] flex-col my-8 p-4 rounded-3xl drop-shadow-lg text-white font-semibold hover:bg-cyan-500 hover:-translate-y-5 hover:-translate-x-5 transition space-y-3 cursor-pointer'
+                    className={
+                      (idea.status == 'todo'
+                        ? 'bg-red-600 hover:bg-red-500'
+                        : idea.status == 'inprogress'
+                        ? 'bg-yellow-500 hover:bg-yellow-400'
+                        : 'bg-green-600 hover:bg-green-500') +
+                      ` min-w-[160px] w-[160px] h-[150px] flex-col my-8 p-4 rounded-3xl drop-shadow-lg text-white font-semibold hover:-translate-y-5 hover:-translate-x-5 transition space-y-3 cursor-pointer`
+                    }
                   >
-                    <p className='text-lg'>{idea.title}</p>
-                    <p className='text-sm font-light'>{idea.description}</p>
+                    <p className='text-lg truncate'>{idea.title}</p>
+                    <p className='text-sm font-light overflow-y-hidden h-4/6'>
+                      {idea.description}
+                    </p>
                   </Link>
                 );
               })}
